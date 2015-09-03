cda2f74517da12393f9b0d9de73b0c924cb061c0
shuhei <kawashu0723@gmail.com>
2015-09-03 17:29:56 +0900

    Initial Commit

---
+++ nobishiro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nobishiro
+//
+//  Created by akaimo on 2015/09/03.
+//  Copyright (c) 2015年 akaimo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
