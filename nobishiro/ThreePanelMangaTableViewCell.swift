5585a61ee6d357384dcac67c6a31b36d969e26f5
Shigure Shimotori <komata.h.aa@m.titech.ac.jp>
2015-09-03 20:04:58 +0900

    Add cells in timeline

---
+++ nobishiro/ThreePanelMangaTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ThreePanelMangaTableViewCell.swift
+//  nobishiro
+//
+//  Created by S-Shimotori on 9/3/15.
+//  Copyright (c) 2015 akaimo. All rights reserved.
+//
+
+import UIKit
+
+class ThreePanelMangaTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- nobishiro/ViewController.swift
+++ nobishiro/ViewController.swift
@@ -8,18 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet private weak var timelineTableView: UITableView!
+
+    let sampleIdentifier = ["TwoPanelManga", "ThreePanelManga", "FourPanelManga"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        timelineTableView.dataSource = self
+        timelineTableView.delegate = self
+        timelineTableView.allowsSelection = false
+
+        for identifier in sampleIdentifier {
+            timelineTableView.registerNib(UINib(nibName: "\(identifier)TableViewCell", bundle: nil), forCellReuseIdentifier: identifier)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //MARK: TODO
+        return 10
     }
 
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        //MARK: TODO
+        switch indexPath.row % 3 {
+        case 1:
+            return 320
+        case 2:
+            return 474
+        default:
+            return 628
+        }
+    }
 
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+
+        //MARK: TODO
+        let cell: UITableViewCell!
+        switch indexPath.row % 3 {
+        case 1:
+            cell = timelineTableView.dequeueReusableCellWithIdentifier(sampleIdentifier[0]) as! TwoPanelMangaTableViewCell
+        case 2:
+            cell = timelineTableView.dequeueReusableCellWithIdentifier(sampleIdentifier[1]) as! ThreePanelMangaTableViewCell
+        default:
+            cell = timelineTableView.dequeueReusableCellWithIdentifier(sampleIdentifier[2]) as! FourPanelMangaTableViewCell
+        }
+        return cell
+    }
 }
 
