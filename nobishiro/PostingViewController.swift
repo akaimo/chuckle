a7e6e8deeb3519a258db0733ea456cbc2f028dd0
shuhei <kawashu0723@gmail.com>
2015-09-03 17:36:41 +0900

    add PostingViewController

---
+++ nobishiro/PostingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PostingViewController.swift
+//  nobishiro
+//
+//  Created by akaimo on 2015/09/03.
+//  Copyright (c) 2015年 akaimo. All rights reserved.
+//
+
+import UIKit
+
+class PostingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
