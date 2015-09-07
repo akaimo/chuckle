3ccdc15c8d2c73829d7dc22ba5fccf31b325763a
Shigure Shimotori <komata.h.aa@m.titech.ac.jp>
2015-09-07 11:38:44 +0900

    Add files

---
+++ nobishiro/Favorite.swift
@@ -0,0 +1,9 @@
+//
+//  Favorite.swift
+//  nobishiro
+//
+//  Created by S-Shimotori on 9/7/15.
+//  Copyright (c) 2015 akaimo. All rights reserved.
+//
+
+import Foundation
---
+++ nobishiro/FavoritesData.swift
@@ -0,0 +1,9 @@
+//
+//  FavoritesData.swift
+//  nobishiro
+//
+//  Created by S-Shimotori on 9/7/15.
+//  Copyright (c) 2015 akaimo. All rights reserved.
+//
+
+import Foundation
