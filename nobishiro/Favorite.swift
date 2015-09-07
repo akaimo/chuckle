06844112bc97f9297b12507b9adf09d3a3421512
Shigure Shimotori <komata.h.aa@m.titech.ac.jp>
2015-09-07 14:42:18 +0900

    Add models for GET

--- nobishiro/Favorite.swift
+++ nobishiro/Favorite.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+import Himotoki
+
+struct Favorite: Decodable {
+    let work_id: Int
+
+    static func decode(e: Extractor) -> Favorite? {
+        let create = { Favorite(work_id: $0) }
+
+        return build(create)(
+            e <| "work_id"
+        )
+    }
+}
