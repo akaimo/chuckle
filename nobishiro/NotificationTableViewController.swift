719026f954ac5a0eea355b80d78516d61f746f16
yujif <yujifujisaka@gmail.com>
2015-09-09 00:45:10 +0900

    NotificationTableVC: add DataSource, Delegate

--- nobishiro/NotificationTableViewController.swift
+++ nobishiro/NotificationTableViewController.swift
@@ -7,15 +7,29 @@
 //
 
 import UIKit
+import Himotoki
+import Alamofire
 
-class NotificationTableViewController: UIViewController {
+class NotificationTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet private weak var notificationTableView: UITableView!
-  
+    private let refreshControl = UIRefreshControl()
+    private var notification: [Notification] = [] {
+      didSet {
+        notificationTableView.reloadData()
+      }
+    }
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        notificationTableView.dataSource = self
+        notificationTableView.delegate = self
+        notificationTableView.allowsSelection = false
+        loadNotifications()
+        refreshControl.addTarget(self, action: "loadNotifications", forControlEvents: .ValueChanged)
+        notificationTableView.addSubview(refreshControl)
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,7 +40,60 @@ class NotificationTableViewController: UIViewController {
     override func loadView() {
       if let view = UINib(nibName: "NotificationTableViewController", bundle: nil).instantiateWithOwner(self, options: nil).first as? UIView {
         self.view = view
-      }
+        }
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notification.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+
+  
+    func loadNotifications() {
+        
+        var userID: AnyObject!
+        var platform = platformName()
+        if platform == "x86_64" {
+            // simulator
+            userID = 1
+        } else {
+            // 実機処理
+            let ud = NSUserDefaults.standardUserDefaults()
+            userID = ud.objectForKey("userID")
+        }
+        
+        Alamofire.request(.GET, "http://yuji.website:3001/api/favorite/\(userID)")
+        .responseJSON { request, response, JSON, error in
+            println(request)
+            println(response)
+            println(JSON)
+            println(error)
+            switch (JSON, error) {
+            case (.Some(let json), .None):
+                if let notificationsData: NotificationsData = decode(json) {
+                    self.notification = notificationsData.data
+                }
+            case (.None, .Some):
+                println(error)
+            default:
+                println("both json and error are nil!")
+            }
+            self.refreshControl.endRefreshing()
+        }
+    }
+    
+    func platformName() -> String {
+        var size: size_t = 0;
+        sysctlbyname("hw.machine", nil, &size, nil, 0)
+        var machine = UnsafeMutablePointer<CChar>(malloc(size))
+        sysctlbyname("hw.machine", machine, &size, nil, 0)
+        var platformName = NSString(CString: machine, encoding: NSUTF8StringEncoding)
+        free(machine)
+        
+        return platformName! as String
     }
 
     /*
