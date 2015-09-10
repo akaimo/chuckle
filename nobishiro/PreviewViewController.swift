14c4f73c6218230069b7d368bd044f38700cc744
shuhei <kawashu0723@gmail.com>
2015-09-11 01:21:02 +0900

    add post title limit

--- nobishiro/PreviewViewController.swift
+++ nobishiro/PreviewViewController.swift
@@ -17,6 +17,10 @@ class PreviewViewController: UIViewController, UITableViewDataSource, UITableVie
     internal var imgArray: [Int]!
     internal var materials: [Material]!
     private var postTitle = ""
+    private let maxLength = 20
+    private var previousText = ""
+    private var lastReplaceRange: NSRange!
+    private var lastReplacementString = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -156,12 +160,35 @@ class PreviewViewController: UIViewController, UITableViewDataSource, UITableVie
     }
     
     func textViewDidChange(textView: UITextView) {
+        if textView.markedTextRange != nil {
+            return
+        }
+        
+        if count(textView.text) > maxLength {
+            var offset = maxLength - count(textView.text)
+            var replacementString = (lastReplacementString as NSString).substringToIndex(count(lastReplacementString) + offset)
+            var text = (previousText as NSString).stringByReplacingCharactersInRange(lastReplaceRange, withString: replacementString)
+            var position = textView.positionFromPosition(textView.selectedTextRange!.start, offset: offset)
+            var selectedTextRange = textView.textRangeFromPosition(position, toPosition: position)
+            
+            textView.text = text
+            textView.selectedTextRange = selectedTextRange
+        }
+        
         self.postTitle = textView.text
         titleCheck()
     }
     
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        self.previousText = textView.text
+        self.lastReplaceRange = range
+        self.lastReplacementString = text
+        
+        return true
+    }
     
     
+    // MARK: -
     func titleCheck() {
         let str = postTitle.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
         let sstr = str.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
